7951da1f6cb822e6175733408514406164e2a1bd
eser2135 <eser.akkurt@hotmail.com>
2021-10-03 03:01:51 -0400

    added for loop solution without map

--- acronym/acronym.js
+++ acronym/acronym.js
@@ -1,5 +1,19 @@
 function acronym(str) {
-    var words = str.split(" ");
-    console.log(((words.map(x => x[0])).join('')).toUpperCase())
+    var words = str.split(" ")
+    var len = words.length
+    // with if loop
+    var newstr = ""
+    var acro = ""
+    for(let i=0; i<=len-1; i++){
+      newstr = words[0].split("")
+      acro += newstr[0] 
+      words.shift()
     }
-    acronym("united states america")
+      console.log(acro.toUpperCase())
+  
+    // with map function
+    // var word = ((words.map(x=>x[0])).join("").toUpperCase())
+    // console.log(word)
+  }
+  
+  acronym('united states america')
